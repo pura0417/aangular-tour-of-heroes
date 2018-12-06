d24d7bdafc66d157e1c31f482f9765acba3b53fd
Puja Rani <pura0417@netcracker.com>
2018-12-06 17:28:09 +0530

    First commit

---
+++ src/app/heroes/dummy-heroes.ts
@@ -0,0 +1,18 @@
+
+import { Hero } from './hero';
+
+export const Heroes: Hero[] = [
+
+  { id: 1, name: 'Amir' },
+  { id: 2, name: 'sharukh' },
+  { id: 5, name: 'Karan' },
+  { id: 3, name: 'Rahul' },
+  { id: 6, name: 'kk' },
+  { id: 7, name: 'Tiger' },
+  { id: 8, name: 'Sushant' },
+  { id: 9, name: 'Varun' },
+  { id: 11, name: 'Shiv' },
+  { id: 12, name: 'Akshay' },
+  { id: 13, name: 'Govind' }
+
+];
---
+++ src/app/heroes/hero.ts
@@ -0,0 +1,5 @@
+export class Hero {
+
+  name: String;
+  id: number;
+}
